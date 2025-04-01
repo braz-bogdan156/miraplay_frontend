d09c2874549f71c656d80861c626fb2bb8d07ebe
Богдан <brazbogdan@gmail.com>
2025-04-01 21:08:08 +0300

    updated commit

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,22 +1,24 @@
 export interface Game {
-    id: string;
-    systemGameName: string;
-    commonGameName: string;
-    gameDescription: string;
-    gameLaunchers: string[];
-    gameImage: string;
-    gameClass: string;
-    genre: string;
-    inTop: boolean;
-    releaseDate: string;
-    publisher: string;
-    gameVideoUrl: string;
-    gameImages: string[];
-    exactingness: string;
-    createdAt: string;
-    updatedAt: string;
-    gameBoxArt: string;
-    gameLogo: string;
-    gameVideoLauncherUrl: string;
-    gameHero: string;
-  }
+  id: string;
+  systemGameName: string;
+  commonGameName: string;
+  gameDescription: string;
+  gameLaunchers: string[];
+  gameImage: string;
+  gameClass: string;
+  genre: string;
+  inTop: boolean;
+  releaseDate: string;
+  publisher: string;
+  gameVideoUrl: string;
+  gameImages: string[];
+  exactingness: string;
+  createdAt: string;
+  updatedAt: string;
+  gameBoxArt: string;
+  gameLogo: string;
+  gameVideoLauncherUrl: string;
+  gameHero: string;
+}
+
+
